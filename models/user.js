5509268b1b69fd725da151db82ed2080f7907dc4
pushnoy100500 <romanenko.igor2014@outlook.com>
2015-12-28 17:36:12 -0500

    prettyfying
    basic error handling

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://localhost/nodeauth');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 //User schema
